461085269da517d0693921b41101293d09778f86
Anthony Sistilli <sistillianthony@gmail.com>
2024-12-02 17:11:07 -0500

    add some logging

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,49 +1,42 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-import {
-  type CookieOptions,
-  createServerClient
-} from '@supabase/ssr'
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get('code')
+  const { searchParams, origin } = new URL(request.url);
+  console.log({ origin });
+  const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  console.log('inside this confirm/route')
-  const next = searchParams.get('next') ?? '/'
-  console.log('inside this confirm/route')
+  console.log("inside this confirm/route");
+  const next = searchParams.get("next") ?? "/";
+  console.log("inside this confirm/route");
   if (code) {
-    const cookieStore = cookies()
+    const cookieStore = cookies();
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
           get(name: string) {
-            return cookieStore.get(name)?.value
+            return cookieStore.get(name)?.value;
           },
-          set(
-            name: string,
-            value: string,
-            options: CookieOptions
-          ) {
-            cookieStore.set({ name, value, ...options })
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options });
           },
           remove(name: string, options: CookieOptions) {
-            cookieStore.delete({ name, ...options })
-          }
-        }
+            cookieStore.delete({ name, ...options });
+          },
+        },
       }
-    )
-    const { error } =
-      await supabase.auth.exchangeCodeForSession(code)
+    );
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/error`)
+  return NextResponse.redirect(`${origin}/auth/error`);
 }
