e1122aa0eee59d277f9dede8d091bc8897c04239
Anthony Sistilli <sistillianthony@gmail.com>
2024-12-02 17:08:15 -0500

    fix dis

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,11 @@ export function cn(...inputs: ClassValue[]) {
 
 // Get the base URL for API calls
 export function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    // Browser should use relative path
+    return "";
+  }
+
   if (process.env.VERCEL_URL) {
     // Reference for vercel.com
     return `https://${process.env.VERCEL_URL}`;
